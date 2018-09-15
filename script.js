5b9457d8aedbaab09e29b694077a9ae3e22702a6
S-HIROTO-THOMAS\hiroto0629 <hirotothnew@outlook.jp>
2018-09-15 20:31:33 +0900

    add-animation

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ $(function () {
     $('.newsLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: 800
+            scrollTop: 600
         }, 800);
     });
     $('.bandLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: 500
+            scrollTop: 1250
         }, 800);
     });
-    $('.photoLink').on('click', function (e) {
+    $('.movieLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: 1000
+            scrollTop: 1810
         }, 800);
     });
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 550){
+            $(`.Title-News`).animate({
+                top:`-600px`,
+                opacity: 0
+            },600);
+            
+        }
+    });
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 1200){
+            $(`.Title-Band`).animate({
+                top:`-600px`,
+                opacity: 0
+            },600);
+            
+        }
+    });
+        $(window).scroll(function(){
+        if($(document).scrollTop() > 1800){
+            $(`.Title-Movie`).animate({
+                top:`-600px`,
+                opacity: 0
+            },600);
+            
+        }
+    });
 });
+
