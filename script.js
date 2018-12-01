545dc8a3423fab1fe25480b3ef04720f396188f8
S-HIROTO-THOMAS\hiroto0629 <hirotothnew@outlook.jp>
2018-12-01 17:31:12 +0900

    ローディン３

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ $(function () {
     
     $(window).load(function () {
         // 消えるタイミングはお好みで
-        $('.loading').delay(1500).fadeOut(300);
+        $('.loading').delay(15000).fadeOut(300);
     });
 
     // 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
