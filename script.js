81f997795e398e48e4a7d0c73b09df2d01596aa7
S-HIROTO-THOMAS\hiroto0629 <hirotothnew@outlook.jp>
2018-12-01 17:25:58 +0900

    ローディング２

--- script.js
+++ script.js
@@ -5,16 +5,7 @@ $(function () {
     
     
     
-    $(window).load(function () {
-        // 消えるタイミングはお好みで
-        $('.loading').delay(1500).fadeOut(300);
-    });
-
-    // 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
-    function stopload() {
-        $('.loading').delay(1000).fadeOut(700);
-    }
-    setTimeout('stopload()', 10000);
+    
 
     
     
@@ -70,4 +61,20 @@ $(function () {
 
         }
     });
+    
+    
+    $(window).load(function () {
+        // 消えるタイミングはお好みで
+        $('.loading').delay(1500).fadeOut(300);
+    });
+
+    // 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
+    function stopload() {
+        $('.loading').delay(1000).fadeOut(700);
+    }
+    setTimeout('stopload()', 10000);
+    
+    
+    
+    
 });
