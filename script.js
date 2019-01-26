12c3244036b7f2209a40ebf8827c7e279f2b28e5
S-HIROTO-THOMAS\hiroto0629 <hirotothnew@outlook.jp>
2019-01-26 17:43:18 +0900

    wip bx-slider-responsvie

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 $(function () {
-    
     $('.newsLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
             scrollTop: 600
         }, 800);
     });
-    
+
     $('.bandLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
             scrollTop: 1250
         }, 800);
     });
-    
+
     $('.movieLink').on('click', function (e) {
         e.preventDefault();
         $('html, body').stop().animate({
-            scrollTop: 1810
+            scrollTop: 2010
         }, 800);
     });
-    
+
+    $('#slider').bxSlider({
+    ticker: true,
+    minSlides: 3,
+    maxSlides: 10,
+    slideWidth: 300,
+    slideMargin: 0,
+    speed: 50000
+  });
+
     $(window).scroll(function () {
         if ($(document).scrollTop() > 550) {
             $('.Title-News').animate({
@@ -30,7 +38,7 @@ $(function () {
 
         }
     });
-    
+
     $(window).scroll(function () {
         if ($(document).scrollTop() > 1200) {
             $('.Title-Band').animate({
@@ -40,7 +48,7 @@ $(function () {
 
         }
     });
-    
+
     $(window).scroll(function () {
         if ($(document).scrollTop() > 1800) {
             $('.Title-Movie').animate({
@@ -50,7 +58,7 @@ $(function () {
 
         }
     });
-    
+
     $(window).load(function () {
         // 消えるタイミングはお好みで
         $('.loading').delay(5000).fadeOut(4000);
@@ -61,8 +69,9 @@ $(function () {
         $('.loading').delay(1000).fadeOut(7000);
     }
     setTimeout('stopload()', 1000);
-    
-    
-    
-    
+
+
+
+
+
 });
